c5324db77543a886a386cced2391c83c89320e3d
CharlesCypher <99364836+CharlesCypher@users.noreply.github.com>
2023-08-03 16:42:27 +0100

    replaced useEffect with useLayoutEffect

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,57 +9,61 @@ import SectionSix from "../SectionSix/SectionSix";
 import SectionSeven from "../SectionSeven/SectionSeven";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
 const Main = () => {
   gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-  useEffect(() => {
-    let panels = gsap.utils.toArray(".section"),
-      observer = ScrollTrigger.normalizeScroll(true),
-      scrollTween;
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      let panels = gsap.utils.toArray(".section"),
+        observer = ScrollTrigger.normalizeScroll(true),
+        scrollTween;
 
-    // on touch devices, ignore touchstart events if there's an in-progress tween so that touch-scrolling doesn't interrupt and make it wonky
-    document.addEventListener(
-      "touchstart",
-      (e) => {
-        if (scrollTween) {
-          e.preventDefault();
-          e.stopImmediatePropagation();
-        }
-      },
-      { capture: true, passive: false }
-    );
-
-    function goToSection(i) {
-      scrollTween = gsap.to(window, {
-        scrollTo: { y: i * innerHeight, autoKill: false },
-        onStart: () => {
-          observer.disable(); // for touch devices, as soon as we start forcing scroll it should stop any current touch-scrolling, so we just disable() and enable() the normalizeScroll observer
-          observer.enable();
+      // on touch devices, ignore touchstart events if there's an in-progress tween so that touch-scrolling doesn't interrupt and make it wonky
+      document.addEventListener(
+        "touchstart",
+        (e) => {
+          if (scrollTween) {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+          }
         },
-        duration: 1.1,
-        onComplete: () => (scrollTween = null),
-        overwrite: true,
-      });
-    }
+        { capture: true, passive: false }
+      );
 
-    panels.forEach((panel, i) => {
+      function goToSection(i) {
+        scrollTween = gsap.to(window, {
+          scrollTo: { y: i * innerHeight, autoKill: false },
+          onStart: () => {
+            observer.disable(); // for touch devices, as soon as we start forcing scroll it should stop any current touch-scrolling, so we just disable() and enable() the normalizeScroll observer
+            observer.enable();
+          },
+          duration: 1.1,
+          ease: "Power3.inOut",
+          onComplete: () => (scrollTween = null),
+          overwrite: true,
+        });
+      }
+
+      panels.forEach((panel, i) => {
+        ScrollTrigger.create({
+          trigger: panel,
+          start: "top bottom",
+          end: "+=199%",
+          onToggle: (self) => self.isActive && !scrollTween && goToSection(i),
+        });
+      });
+
+      // just in case the user forces the scroll to an inbetween spot (like a momentum scroll on a Mac that ends AFTER the scrollTo tween finishes):
       ScrollTrigger.create({
-        trigger: panel,
-        start: "top bottom",
-        end: "+=199%",
-        onToggle: (self) => self.isActive && !scrollTween && goToSection(i),
+        start: 0,
+        end: "max",
+        snap: 1 / (panels.length - 1),
       });
     });
-
-    // just in case the user forces the scroll to an inbetween spot (like a momentum scroll on a Mac that ends AFTER the scrollTo tween finishes):
-    ScrollTrigger.create({
-      start: 0,
-      end: "max",
-      snap: 1 / (panels.length - 1),
-    });
+    return () => ctx.revert();
   }, []);
   return (
     <>
